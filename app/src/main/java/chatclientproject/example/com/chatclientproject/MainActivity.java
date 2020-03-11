03e2c41c494c954fe70cf9760faf6ac44c67768c
chungtwo <317281147@qq.com>
2020-03-11 10:31:06 +0800

    First commit

---
+++ app/src/main/java/chatclientproject/example/com/chatclientproject/MainActivity.java
@@ -0,0 +1,13 @@
+package chatclientproject.example.com.chatclientproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
